d2b95807bed1e4362b83c8bf2487885989e0c0dc
NikitaRavindraPatil <nikita.5601patil@gmail.com>
2026-04-29 17:25:33 +0530

    service class added

---
+++ Department_Service/src/main/java/com/app/service/DepartmentService.java
@@ -0,0 +1,10 @@
+package com.app.service;
+
+import com.app.entity.Department;
+
+public interface DepartmentService {
+
+	Department saveDepartment(Department department);
+	
+	Department getDepartmentById(Long departmentId);
+}
