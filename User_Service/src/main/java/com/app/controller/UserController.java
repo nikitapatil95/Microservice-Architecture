3e282a6c611d3b2fc960f378abc1eaaf55ed597f
NikitaRavindraPatil <nikita.5601patil@gmail.com>
2026-04-29 14:00:03 +0530

    controller class added

---
+++ User_Service/src/main/java/com/app/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.app.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.dto.ResponseDTO;
+import com.app.entity.User;
+import com.app.service.UserService;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping
+	public ResponseEntity<User> saveUser(@RequestBody User user){
+		User savedUser = userService.saveUser(user);
+		return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<ResponseDTO> getUser(@PathVariable("id") Long userId){
+		ResponseDTO responeDto = userService.getUser(userId);
+		return ResponseEntity.ok(responeDto);
+	}
+	
+}
